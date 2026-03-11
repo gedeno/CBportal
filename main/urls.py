cae0f3a264fef747a35677f9fe6c33e3e5bbd4b5
gedeno <gedenogeremew@gmail.com>
2026-03-11 12:20:00 +0300

    the aouthontication system is reparid

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import MyLoginView,RegisterView
+from .views import MyLoginView,RegisterView,HomeView ,MyLogoutView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('',RegisterView.as_view(),name = "register"),
+    path('register/',RegisterView.as_view(),name = "register"),
     path('login/',MyLoginView.as_view(),name="login"),
-    path('logout/',LogoutView.as_view(),name="logout")
-    ,
+    path('logout/',LogoutView.as_view(),name="logout"),
+    path('',HomeView.as_view(),name="home"),
+    path('logout/',MyLogoutView.as_view(), name = "logout"),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import FormView
 from django.contrib.auth.views import LoginView,LogoutView
-
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 # Create your views here.
 class RegisterView(FormView):
-    template_name = "register.html"
-    form_class = UserChangeForm
+    template_name = "main/register.html"
+    form_class = UserCreationForm
     success_url = "/login/"
-    def form_invalid(self, form):
+    def form_valid(self,form):
         form.save()
-        return super().form_invalid(form)
+        return super().form_valid(form)
 class MyLoginView(LoginView):
-    template_name = "login.html"
+    template_name = "main/login.html"
+    def get_success_url(self):
+        return "home"
+class HomeView(LoginRequiredMixin,TemplateView):
+    template_name = 'main/home.html'
+class MyLogoutView(LogoutView):
+    next_page = "/login/"
